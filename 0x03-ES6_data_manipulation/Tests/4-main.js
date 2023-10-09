8c0b77b8d047d8dbaf0419440f12a145c6758165
Wanjiru393 <carolciru393@gmail.com>
2023-10-09 11:27:26 +0300

    Updating tests

--- 0x03-ES6_data_manipulation/Tests/4-main.js
+++ 0x03-ES6_data_manipulation/Tests/4-main.js
@@ -1,5 +1,5 @@
-import getListStudents from './0-get_list_students';
-import updateStudentGradeByCity from './4-update_grade_by_city';
+import getListStudents from '../0-get_list_students';
+import updateStudentGradeByCity from '../4-update_grade_by_city';
 
 console.log(updateStudentGradeByCity(getListStudents(), 'San Francisco', [{ studentID: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
 
