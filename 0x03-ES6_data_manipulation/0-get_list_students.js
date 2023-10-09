acaa5bab479ab97ecea147ec45685eadca7d39e8
Wanjiru393 <carolciru393@gmail.com>
2023-10-09 11:44:22 +0300

    format

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
-  
-  export default function getListStudents() {
-     return students;
-  }
+   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+   { id: 2, firstName: 'James', location: 'Columbia' },
+   { id: 5, firstName: 'Serena', location: 'San Francisco' }
+];
+
+export default function getListStudents() {
+   return students;
+}
