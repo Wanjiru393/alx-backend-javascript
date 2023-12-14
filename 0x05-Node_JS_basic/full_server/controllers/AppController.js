bbc601b7d0fe6af1590fc4e6bc58e82466febfda
Wanjiru393 <carolciru393@gmail.com>
2023-12-14 13:33:02 +0300

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+/**
+ * Contains the miscellaneous route handlers.
+ * @author Wanjiru Charity */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
